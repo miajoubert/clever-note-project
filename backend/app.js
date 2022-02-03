ac111af535dfa7d1974673c77680828598340478
Mia <mia.joubert@gmail.com>
2022-02-03 12:56:44 -0600

    tested error handlers

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const routes = require('./routes')
 
@@ -32,6 +33,32 @@ app.use(csurf({
 app.use(routes);
 
 
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource could not be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource could not be found."];
+  err.status = 404;
+  next(err)
+});
+
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, _req, res, next) => {
+  res.status(err.status || 500);
+  console.log(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.erros,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 
 module.exports = app;
