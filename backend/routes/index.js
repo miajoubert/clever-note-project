b21b447e5fb5d09c58bf298236e59ea69d9353c0
Mia <mia.joubert@gmail.com>
2022-02-03 18:28:50 -0600

    backend setup complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,7 +7,7 @@ router.use('/api', apiRouter)
 
 router.get('/', function (req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!')
+  res.send('Solo application landing page!')
 })
 
 module.exports = router;
