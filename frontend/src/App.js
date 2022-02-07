62433588b683473ae48ad00d1c20a7b7533f3038
Mia <mia.joubert@gmail.com>
2022-02-06 21:00:32 -0600

    logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import logo from '../public'
 import LoginFormPage from './components/LoginFormPage'
 
 function App() {
   return (
     <nav>
+      <a>
+        <img src='logo'
+      </a>
       <div>Welcome to Clever Note!</div> {/*remember to remove*/}
       <Switch>
         <Route path='/login'>
