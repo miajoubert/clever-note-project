795385d32400eefc3a9ca1b42c0a2316da3898e8
Mia <mia.joubert@gmail.com>
2022-02-08 20:13:31 -0600

    finished read of 1st CRUD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
           <Route path="/signup">
             <SignupFormPage />
           </Route>
-          <Route path="/notes">
+          <Route path={["/notes", "/notes/:noteId"]}>
             <NotesPage />
           </Route>
           {/* <Route path="/notebooks">
---
+++ frontend/src/components/Notes/NoteDetail.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState, Route } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import * as sessionActions from "../../store/session";
+import FloatingButton from "../FloatingButton";
+import { listNotes, noteDetails } from "../../store/notes";
+
+import './Notes.css'
+
+const NoteDetail = () => {
+  const { noteId } = useParams();
+
+  const note = useSelector(state => state.notes[noteId]);
+  const session = useSelector(state => state.session);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(noteDetails(noteId))
+  }, [noteId])
+
+  const userId = session.user.id;
+
+  return (
+    <>
+      <div className="noteDetailBackground" >
+        <div className="title">{note.title}</div>
+        <div className="content">{note.content}</div>
+        <div className="centered">
+          {new Date(note.updatedAt).toDateString()} {new Date(note.updatedAt).getHours()}:{new Date(note.updatedAt).getMinutes()}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NoteDetail;
